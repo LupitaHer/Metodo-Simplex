ddde63eebe9e630f1700c35304357c579ad87dd9
LupitaHer <146145745+LupitaHer@users.noreply.github.com>
2023-10-10 08:49:24 -0600

    Vista.java

--- src/main/java/GUI/Vista.java
+++ src/main/java/GUI/Vista.java
@@ -343,8 +343,87 @@ jTextArea1.append(""El pivote es:¨+pivo);
             int h = 0;
             for(int i = 1; i < fila; i++){
                 v1[h] = v[i][columna1 +1] / v [i][c];
+                jTextArea1.append(" " + v[i][columna - 1] + " / " + v[i][c] + " = " + v1[h] + " \n");
+                h++;
+            }
+            mayor = v1[0];
+            for(int i; i< fila - 1; i++){
+                if(v1[i]<= menor){
+                    mayor = v1 [i];
+                    f= i+1;
+                }
+            }
+            jTextArea1.append("el mayor de la division es :" + mayor + " \n");
+            double pivo = v [f][c];
+            jTextArea1.append ("el pivote es "+ pivo);
+
+            jTextArea1.append("\nproceso de convertir el pivote en 1\n dividiendo toda la fila en el pivote\n");
+            for (int i = 0; i< columna; i++) {
+                double va  = v [f][i];
+                v[f][i] = v[f][i] / pivo;
+                jTextArea1.append("" + va + "/" + pivo + "=" + v [f][i] + " \n");
+            }
+            for (int = 1; i< fila; i++){
+                if(i == f){
+                    v2[i] = "x" + (c + 1);
+                    v3[i] = c + 1;
+                    v4[i] = i;
+                }
+                if (v3[i]<= 0){
+                   v2 [i] = "d" + (i);
+                }
+            }
+            jTextAreal.append ("\n proceso\n");
+            for(int i = 0; i< fila; i++){
+                if(i != f){
+                    jTextArea1.append("Convertiendo la columna del pivote en cero, en fila"+ (i+1) +"\n\n");
+                    double guar = 0;
+                    guar = v [i][c];
+                     for (int j = 0; j < columna; j++) {
+                        double vaa = v[i][j];
+                        v[i][j] = guar * v[f][j] + v[i][j];
+                        jTextArea1.append(" " + guar + "*" + v[f][j] + " + " + vaa + " = " + v[i][j] + "\n");
+                }
             }
         }
+            co = 0;
+            for(int i = 0; i< colummna1; i++){
+                if(v[0][i] >= 0){
+                    co++;
+                }
+            }
+            y1 = 1;
+            for(int i = 0; i< columna; i++){
+                if(i<columna1){
+                    jTextArea1.append (("\t x" + (i + 1));
+                }else if (i>= columna1 && i< columna-1){
+                    jTextArea1.append("\t d" + (y1));
+                    y1++;
+                }else{
+                    jTextArea1.append ("\t resul");
+                }
+            }
+            jTextArea1.append("\n");
+            for(int i = 0; i<fila; i++){
+                if(i!=0){
+                    jTextArea1.append(v2[i]);
+                }
+                for (int j = 0; j < columna; j++) {
+                    jTextArea1.append("\t " + l.format(v[i][j]));
+                }
+                jTextArea1.append("\n");
+            }
+            jTextArea1.append("\n\n");
+            }
+            int y2 = 0;
+            jTextArea1.append ("Resultado\n");
+            jTextArea1.append("z = "+v[0][columna-1]+"\n");
+            for(int i = 1; i< fila; i++){
+                char m1[] = v2[i].toCharArray();
+                if (m1[0] == 'x'){
+                    jTextArea1.append("" + v2[i] + "=" + v[v4[i]][columna - 1] + "\n");
+                }
+            }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     public static void main(String args[]) {
